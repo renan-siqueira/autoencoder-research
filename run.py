d3f7df4caede926ba18a99e96884b9cd3fc0bd84
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-10-13 17:38:40 -0300

    feat: create checkpoint feature

--- run.py
+++ run.py
@@ -3,36 +3,37 @@ import torch
 
 from models.autoencoder import Autoencoder
 from utils.dataloader import get_dataloader
-from utils.trainer import train_autoencoder, visualize_reconstructions, save_model, load_model, evaluate_autoencoder
+from utils.trainer import train_autoencoder, visualize_reconstructions, save_checkpoint, load_checkpoint, evaluate_autoencoder
 from settings import settings
 
 
 def main(load_trained_model):
     BATCH_SIZE = 32
     INPUT_DIM = 3 * 64 * 64
-    ENCODING_DIM = 12
-    NUM_EPOCHS = 1000
+    ENCODING_DIM = 64
+    NUM_EPOCHS = 200
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
     dataloader = get_dataloader(settings.DATA_PATH, BATCH_SIZE)
-    model = Autoencoder(INPUT_DIM, ENCODING_DIM).to(device)
 
-    if load_trained_model:
-        trained_model = load_model(model, settings.PATH_SAVED_MODEL, device=device)
-    else:
-        trained_model = train_autoencoder(model, dataloader, NUM_EPOCHS, device=device)
+    model = Autoencoder(INPUT_DIM, ENCODING_DIM).to(device)
+    optimizer = torch.optim.Adam(model.parameters())
+
+    start_epoch = 0
+    if os.path.exists(settings.PATH_SAVED_MODEL):
+        model, optimizer, start_epoch = load_checkpoint(model, optimizer, settings.PATH_SAVED_MODEL, device)
+        print(f"Loaded checkpoint and continuing training from epoch {start_epoch}.")
+
+    if not load_trained_model:
+        for epoch in range(start_epoch, NUM_EPOCHS):
+            train_autoencoder(model, dataloader, device=device)
+            print(f"Epoch [{epoch + 1}/{NUM_EPOCHS}] complete!")
+            save_checkpoint(model, optimizer, epoch, settings.PATH_SAVED_MODEL)
 
     valid_dataloader = get_dataloader(settings.VALID_DATA_PATH, BATCH_SIZE)
-
-    save_path = os.path.join('./', settings.PATH_SAVED_MODEL)
-    save_model(trained_model, save_path)
-    print(f"Model saved to {save_path}")
-
-    avg_valid_loss = evaluate_autoencoder(trained_model, valid_dataloader, device)
+    avg_valid_loss = evaluate_autoencoder(model, valid_dataloader, device)
     print(f"Average validation loss: {avg_valid_loss:.4f}")
-
-    visualize_reconstructions(trained_model, valid_dataloader, num_samples=10, device=device)
+    visualize_reconstructions(model, valid_dataloader, num_samples=10, device=device)
 
 
 if __name__ == "__main__":
