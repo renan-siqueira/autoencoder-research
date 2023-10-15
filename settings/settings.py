13eb9e087341018c109fdd9c8022faeb09607aa1
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-10-15 20:18:11 -0300

    feat: improvements on training and create new models and tools file

--- settings/settings.py
+++ settings/settings.py
@@ -1,4 +1,15 @@
-DATA_PATH = './data/train'
-VALID_DATA_PATH = './data/valid'
-PATH_SAVED_MODEL = './autoencoder_model.pth'
+DATA_PATH = './dataset/train'
+VALID_DATA_PATH = './dataset/valid'
+PATH_SAVED_MODEL = './autoencoder_checkpoint.pth'
+PATH_SAVED_SAMPLES = './training'
 PATH_PARAMS_JSON = './json/params.json'
+
+# COPY RANDOMIC FILES
+COPY_SOURCE_FOLDER = 'C:/Users/Meu Computador/Desktop/projetos/00-Datasets/processed_videos/Jinjer_Vortex_REACTION_ANALYSIS_by_Vocal_Coach_Opera_Singer/original/face'
+
+COPY_DESTINATION_FOLDER = 'dataset/train'
+# COPY_DESTINATION_FOLDER = 'dataset/valid'
+
+COPY_PERCENTAGE_TO_COPY = None
+COPY_RANDOM_MODE = True
+COPY_FIXED_NUMBER_TO_COPY = 1600
