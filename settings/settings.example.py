ee3882a1f48bedbc9b3f69bb29934f65fa22b2f4
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-10-16 14:30:59 -0300

    feat: update .gitignore

---
+++ settings/settings.example.py
@@ -0,0 +1,13 @@
+DATA_PATH = './dataset/train'
+VALID_DATA_PATH = './dataset/valid'
+PATH_SAVED_MODEL = './autoencoder_checkpoint.pth'
+PATH_SAVED_SAMPLES = './training'
+PATH_PARAMS_JSON = './json/params.json'
+
+# COPY RANDOMIC FILES
+COPY_SOURCE_FOLDER = '__source_images_folder_path__'
+
+TRAIN_PERCENTAGE = 0.7
+VALID_PERCENTAGE = 0.3
+
+COPY_FIXED_NUMBER_TO_COPY = 32
